84698dd366d844dd8d1c1412e247beb5d1e9295d
josephaizang <nebjulatechnologies@gmail.com>
2020-03-12 13:26:13 +0100

    added carousel for the homepage

--- src/Steunijos.Web/ViewModels/ContactUs/ContactUsInputModel.cs
+++ src/Steunijos.Web/ViewModels/ContactUs/ContactUsInputModel.cs
@@ -4,6 +4,12 @@ namespace Steunijos.Web.ViewModels.ContactUs
 {
     public class ContactUsInputModel
     {
+        public string FullName { get; set; }
 
+        public string EmailAddress { get; set; }
+
+        public string Message { get; set; }
+
+        public string MessageId { get; set; }
     }
 }
